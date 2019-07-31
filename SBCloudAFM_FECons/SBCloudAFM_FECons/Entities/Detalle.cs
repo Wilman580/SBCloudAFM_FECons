d91a77c0f3a211497f824d124a5d8ac99cf156ad
SONY-ROYAL <wgmatangog@utn.edu.ec>
2019-07-31 10:02:07 -0500

    Creación de la entidades

---
+++ SBCloudAFM_FECons/SBCloudAFM_FECons/Entities/Detalle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBCloudAFM_FECons.Entities
+{
+    public class Detalle
+    {
+        public int CodigoD { get; set; }
+        public int CantidadD { get; set; }
+        public string ProductoD { get; set; }
+        public float PrecioD { get; set; }
+        public float IvaD { get; set; }
+        public float ImporteD { get; set; }
+    }
+}
---
+++ SBCloudAFM_FECons/SBCloudAFM_FECons/Entities/Factura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBCloudAFM_FECons.Entities
+{
+    public class Factura
+    {
+        public string CodigoFact { get; set; }
+        public int RucCI { get; set; }
+        public string Cliente { get; set; }
+        public DateTime FechaFac { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public float SubTotal { get; set; }
+        public float IvaTotal { get; set; }
+        public float Total { get; set; }
+        public string Observacion { get; set; }
+        public List<Detalle> ListaDet { get; set; }
+    }
+}
